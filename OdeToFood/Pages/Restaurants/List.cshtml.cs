990c81a2775c88839bdfb212180cf88df8cb1e20
david <4504352+northernocean@users.noreply.github.com>
2020-06-20 14:02:37 +0000

    Add Restaurant List Razor Page

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"];
+        }
+    }
+}
