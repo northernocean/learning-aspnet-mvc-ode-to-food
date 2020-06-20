685e3c92745ae4a9ff099018d2df89f1cd5103e8
david <4504352+northernocean@users.noreply.github.com>
2020-06-20 23:07:59 +0000

    Linking to Details

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+
+        public Restaurant Restaurant { get; set; }
+
+        public void OnGet(int restaurantId)
+        {
+            Restaurant = new Restaurant { Id = restaurantId};
+
+        }
+    }
+}
