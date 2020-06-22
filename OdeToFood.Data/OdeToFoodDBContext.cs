b34b146915e3461fbff9fd90f117942d71951e58
david <4504352+northernocean@users.noreply.github.com>
2020-06-22 01:09:56 +0000

    Adding DBContext

---
+++ OdeToFood.Data/OdeToFoodDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDBContext : DbContext
+    {
+
+        public OdeToFoodDBContext(DbContextOptions<OdeToFoodDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace OdeToFood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<OdeToFoodDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb")));
             services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
         }
 
