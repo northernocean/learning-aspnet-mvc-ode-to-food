30cb3af001fdd3b9940e4e24ba5279afecb4b252
david <4504352+northernocean@users.noreply.github.com>
2020-06-24 01:04:03 +0000

    Implement Delete page

---
+++ OdeToFood/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        public Restaurant Restaurant { get; set; }
+
+        public DeleteModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+
+            Restaurant = restaurantData.GetById(restaurantId);
+            if(Restaurant is null)
+                return RedirectToPage("/Error");
+            else
+            {
+                return Page();
+            }
+
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            var restaurant = restaurantData.Delete(restaurantId);
+            
+            if(restaurant is null)
+                return RedirectToPage("/Error");
+            else
+            {
+                restaurantData.Commit();
+                TempData["Message"] = $"{restaurant.Name} deleted.";
+                return RedirectToPage("./List");
+            }
+        }
+    
+    }
+}
